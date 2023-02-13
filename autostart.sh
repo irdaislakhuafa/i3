1ab05ef849be1d3043366c4dc6af0cee474595d9
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-02-14 00:37:51 +0700

    recover brightness

--- autostart.sh
+++ autostart.sh
@@ -6,6 +6,7 @@ feh --bg-scale /media/My_Drive/My_Config/wallpaper/wallpaper.* &!
 # random wallpaper
 #feh --randomize --bg-fill <yourpath>/*
 
+brightnessctl set 20% -r
 
 # To set Gamma
 xgamma -rgamma 0.40 -ggamma 0.50 -bgamma 0.80 &!
